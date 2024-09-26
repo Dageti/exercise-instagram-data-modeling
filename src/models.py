394bd7367608ca64c462967bd5bf4cf10219f3e8
Matías Rivas <87586371+Dageti@users.noreply.github.com>
2024-09-26 16:11:26 +0000

    corregida relacion erronea

--- src/models.py
+++ src/models.py
@@ -15,12 +15,12 @@ class MediaType(enum.Enum):
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(20), unique=True, nullable=False)
-    firstname = Column(String(20), unique=False, nullable=False)
-    lastname = Column(String(20), unique=False, nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    firstname = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
 
-    #relationships
+    # relationship
     post = relationship('Post', backref='user')
     comments = relationship('Comment', backref='author')
 
@@ -28,43 +28,42 @@ class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     type = Column(Enum(MediaType), nullable=False)
-    url = Column(String(200), nullable=False)
-    post_id = Column(Integer, ForeignKey('post.id'),nullable=False)
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
-    # Relationships
+    # relationship
     post = relationship('Post', backref='media')
-
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
+    # relationship
+    comments = relationship('Comment', backref='post')
+    media = relationship('Media', backref='post')
+
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(300), nullable=False)
+    comment_text = Column(String, nullable=False)
     author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
-  
+
+    # relationship
 
 class Follower(Base):
     __tablename__ = 'follower'
-
     user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     
-    # relationships
-    user_from = relationship('user', foreign_keys=[user_from_id], backref='following')
-    user_to = relationship('user', foreign_keys=[user_to_id], backref='followers')
+    # relationship
+    user_from = relationship('User', foreign_keys=[user_from_id], backref='following')
+    user_to = relationship('User', foreign_keys=[user_to_id], backref='followers')
 
-    def to_dict(self):
-        return {}
-
-## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
